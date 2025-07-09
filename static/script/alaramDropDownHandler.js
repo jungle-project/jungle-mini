c2db5fbc786627e0eb0b12e7e629df19c0fc16a8
kimhyunso <gustn9515@gmail.com>
2025-07-09 17:56:57 +0900

    feat: 메인 페이지 dropbox 토글 및 회원가입 페이지 dropbox 토글 수정

--- static/script/alaramDropDownHandler.js
+++ static/script/alaramDropDownHandler.js
@@ -1,9 +1,4 @@
 function alaramDropDownHandler(type) {
     const alaramDropBoxContent = document.getElementById('alaramDropBoxContent');
-
-    if (type == 'visible') {
-        alaramDropBoxContent.classList.add('hidden');
-    } else {
-        alaramDropBoxContent.classList.remove('hidden');
-    }
+    alaramDropBoxContent.classList.toggle('hidden');
 }
--- static/script/rendering.js
+++ static/script/rendering.js
@@ -7,13 +7,13 @@
 
       pageUsers.forEach(user => {
         grid.innerHTML += `
-    <div class="bg-white rounded shadow p-4 text-center">
-      <img src="${user.photo}" alt="${user.name}" class="mx-auto mb-2 rounded-full w-24 h-24 object-cover" />
-      <h2 class="text-lg font-bold">${user.name} (${user.praises})</h2>
-      <button onclick="addPraise('${user.name}')" class="bg-gray-600 text-white px-3 py-1 rounded text-sm mr-1">칭찬</button>
-      <button onclick="goProfile('${user.name}')" class="bg-gray-600 text-white px-3 py-1 rounded text-sm">보기</button>
-    </div>
-  `;
+          <div class="bg-white rounded shadow p-4 text-center">
+            <img src="${user.photo}" alt="${user.name}" class="mx-auto mb-2 rounded-full w-24 h-24 object-cover" />
+            <h2 class="text-lg font-bold">${user.name} (${user.praises})</h2>
+            <a href="/" class="bg-gray-600 text-white px-3 py-1 rounded text-sm mr-1">칭찬</a>
+            <a href="/" class="bg-gray-600 text-white px-3 py-1 rounded text-sm">보기</a>
+          </div>
+        `;
       });
 
       renderPagination();
